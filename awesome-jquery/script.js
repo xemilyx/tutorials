73ff73f4f707e5a4e27967db96d5f53f779e8c5f
Emily Brown <emilyruthbrown@gmail.com>
2017-09-08 23:10:37 +0000

    adding version 3 with stylesheet and classes

--- awesome-jquery/script.js
+++ awesome-jquery/script.js
@@ -16,8 +16,26 @@ $(function() {
     
     $("button").click(function() {
         console.log("You clicked on the button!")
-    })
+    });
     
     $("input").on("change blur focus", function() {
         console.log("You did something to the input field!")
-    })
+    });
+    
+    $("h3").fadeOut(500);
+    $("h3").fadeIn(1500);
+    
+    $("p").toggle(2000);
+    $("p").toggle("normal").addClass("blue");
+    
+    $("h3").hide();
+    $("h3").fadeIn(2000, function() {
+        $(this).addClass("blue");
+    });
+    
+    $("button").click(function() {
+        $("h3").stop();
+        console.log("stop!");
+    });
+    
+    //there are two h3 functions, so click on the button twice to stop the second one.
