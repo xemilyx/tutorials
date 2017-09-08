b788ca2939bf5717241be9884865f0e3073a58ec
Emily Brown <emilyruthbrown@gmail.com>
2017-09-08 22:47:16 +0000

    adding jquery version 2

--- awesome-jquery/script.js
+++ awesome-jquery/script.js
@@ -1,5 +1,23 @@
-$(document).ready(function() {
-    $("h3").click(function() {
-        $("button").hide();
+$(function() {
+    $("p").on(
+        {
+        "click": function() {
+            console.log("You clicked the paragraph!")
+            },
+        "mouseover": function() {
+            console.log("You hovered on the paragraph!")
+             }
+        });
     });
-});
+    
+    $("h3").on("click", function() {
+        console.log("You clicked on the heading!");
+    });
+    
+    $("button").click(function() {
+        console.log("You clicked on the button!")
+    })
+    
+    $("input").on("change blur focus", function() {
+        console.log("You did something to the input field!")
+    })
